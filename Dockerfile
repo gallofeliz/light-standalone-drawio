3a8bd4b878fc5557cceeb7ddeb101520b0070a04
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-11-17 19:52:40 +0100

    some changes

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
 FROM alpine
 
-RUN apk add --no-cache nginx-mod-http-dav-ext nginx gettext
+RUN apk add --no-cache nginx-mod-http-dav-ext nginx gettext s6
 
 EXPOSE 80
 
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY nginx.conf /etc/nginx/nginx.template.conf
 
 WORKDIR /app
 
-COPY index.html empty.xml run.sh ./
+COPY index.html run.sh ./
 
-RUN chmod +x run.sh
+RUN chmod +x run.sh && rm /etc/nginx/nginx.conf
 
 VOLUME /data
 
--- run.sh
+++ run.sh
@@ -1,8 +1,57 @@
 #!/bin/sh
 
-if [ ! -f /data/data.xml ]; then
-    cp ./empty.xml /data/data.xml
-    chown -R nginx:nginx /data
+if [ ! -f /etc/nginx/nginx.conf ]; then
+    echo "Configure Service"
+
+    PUID=${PUID}
+    PGID=${PGID}
+
+    export USER
+    export GROUP
+
+    if [ -n "$PGID" ]; then
+        GROUP=$(getent group $PGID | cut -d: -f1)
+        if [ -z $GROUP  ]; then
+            echo "Creating group"
+            addgroup -g $PGID group
+            GROUP=group
+        fi
+    fi
+
+    if [ -n "$PUID" ]; then
+        USER=$(getent passwd $PUID | cut -d: -f1)
+        if [ -z $USER ]; then
+            echo "Creating user"
+            if [ -n "$PGID" ]; then
+                adduser --disabled-password --uid $PUID --ingroup group user
+            else
+                adduser --disabled-password --uid $PUID user
+            fi
+        fi
+        USER=user
+    else
+        USER=nginx
+    fi
+
+    export LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"'
+
+    envsubst < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
+    if [ -n $GROUP ]; then
+        CAN_WRITE=$(s6-setuidgid $PUID:$PGID sh -c 'test -w /data && echo 1')
+    else
+        CAN_WRITE=$(s6-setuidgid $PUID sh -c 'test -w /data && echo 1')
+    fi
+
+    if [ -z $CAN_WRITE ]; then
+        echo "Unable to write, setting chown"
+        if [ -n $GROUP ]; then
+            chown $USER:$GROUP /data
+        else
+            chown $USER /data
+        fi
+    fi
+
 fi
 
 echo "Start !"
